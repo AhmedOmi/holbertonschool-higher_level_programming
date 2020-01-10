6b8625a9958be9f2f880999871dd6733ad714faf
AhmedOmi <54010086+AhmedOmi@users.noreply.github.com>
2020-01-10 21:19:49 +0100

    Update 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,13 +6,10 @@ def list_division(my_list_1, my_list_2, list_length):
             count = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
-            count = 0
         except ZeroDivisionError:
             print("division by 0")
-            count = 0
         except IndexError:
             print("out of range")
-            count = 0
         finally:
             newlist.append(count)
     return newlist
