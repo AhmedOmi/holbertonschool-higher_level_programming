aba8dc75026338e26493eae432e65872339b16ec
ahmedomi <ahmedomarmiledi@gmail.com>
2020-01-12 23:51:56 +0000

    0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+This module is 
+add_integer(a, b)  adds two integers
+and return an integer 
+'''
+def add_integer(a, b=98):
+        '''
+        function add integer.
+        '''
+        if type(a) not in (float, int):
+            raise TypeError('a must be an integer')
+        if type(b) not in (float, int):
+            raise TypeError('b must be an integer')
+        return int(a) + int(b)
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
