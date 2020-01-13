ae5b44669904ea019d7b7cbb30474d6f4217f672
ahmedomi <ahmedomarmiledi@gmail.com>
2020-01-13 22:00:58 +0000

    3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def say_my_name(first_name, last_name=""):
+        '''
+        function say my name 
+        '''
+        if type(first_name) != str:
+            raise TypeError("first_name must be a string")
+        if type(last_name) != str:
+            raise TypeError("last_name must be a string")
+        print("My name is {:s} {:s}".format(first_name, last_name))
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/3-say_my_name.txt")
