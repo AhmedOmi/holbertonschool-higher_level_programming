6352df5eb092143889e0440714c67eee04bcedde
ahmedomi <ahmedomarmiledi@gmail.com>
2020-01-13 22:41:10 +0000

    5-text_indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+ text_indentation
+'''
+
+
+def text_indentation(text):
+    if not type(text) == str:
+        raise TypeError("text must be a string")
+    for string in ".?:":
+        text = (string + "\n\n").join([i.strip(" ") for i in text.split(string)])
+
+    print(text, end="")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
