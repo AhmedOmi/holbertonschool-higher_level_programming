4af96db45bae43dcfb6c5403bcbe7ea65b470af9
ahmedomi <ahmedomarmiledi@gmail.com>
2020-01-20 15:28:11 +0000

    3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
         '''
-        function say my name 
+        function say my name
         '''
         if type(first_name) != str:
             raise TypeError("first_name must be a string")
