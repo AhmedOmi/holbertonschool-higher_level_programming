ea41c285552d07a9a9c6591d3feb6139aae3f2da
ahmedomi <ahmedomarmiledi@gmail.com>
2020-01-20 15:35:26 +0000

    4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""
+'''
 print_square
-"""
+'''
+
+
 def print_square(size):
-        '''
-        function print square
-        '''
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        print((("#" * size + "\n") * size), end="") 
+    '''
+    function print square
+    '''
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    print((("#" * size + "\n") * size), end="")
+
+
 if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/4-print_square.txt")
