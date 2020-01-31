ca0bf4c6ae2515bd07efefbfcbc5abc162c51654
ahmedomi <ahmedomarmiledi@gmail.com>
2020-01-31 08:49:35 +0000

    class rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,7 +6,9 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    ''' class rectangle '''
     def __init__(self, width, height, x=0, y=0, id=None):
+        ''' funtion constructor '''
         self.width = width
         self.height = height
         self.x = x
@@ -15,32 +17,40 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        ''' function prority '''
         return self.__x
 
     @property
     def y(self):
+        ''' function prority '''
         return self.__y
 
     @property
     def width(self):
+        ''' function prority '''
         return self.__width
 
     @property
     def height(self):
+        ''' function prority '''
         return self.__height
 
     @x.setter
     def x(self, value):
+        ''' function setter '''
         self.__x = value
 
     @y.setter
     def y(self, value):
+        ''' function setter '''
         self.__y = value
 
     @width.setter
     def width(self, value):
+        ''' function setter '''
         self.__width = value
 
     @height.setter
     def height(self, value):
+        ''' function setter '''
         self.__height = value
