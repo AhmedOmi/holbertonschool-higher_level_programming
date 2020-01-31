db3529fd30f617029657788d1751982ffe8e67d6
ahmedomi <ahmedomarmiledi@gmail.com>
2020-01-31 09:36:37 +0000

    message

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,6 +7,7 @@ from models.base import Base
 
 class Rectangle(Base):
     ''' class rectangle '''
+
     def __init__(self, width, height, x=0, y=0, id=None):
         ''' funtion constructor '''
         self.width = width
@@ -38,19 +39,35 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         ''' function setter '''
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be > 0")
         self.__x = value
 
     @y.setter
     def y(self, value):
         ''' function setter '''
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be > 0")
         self.__y = value
 
     @width.setter
     def width(self, value):
         ''' function setter '''
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @height.setter
     def height(self, value):
         ''' function setter '''
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be > 0")
         self.__height = value
