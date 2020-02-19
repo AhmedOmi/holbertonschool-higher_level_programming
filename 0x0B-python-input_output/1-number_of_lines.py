7b7cfd4b1ac0896b95ff1947aa4a2077636a26ca
ahmedomi <ahmedomarmiledi@gmail.com>
2020-02-19 12:31:43 +0000

    task 1

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+what is the number of line in the file
+"""
+
+
+def number_of_lines(filename=""):
+    """ function to return number of line from file """
+    with open(filename, "r", encoding="UTF-8") as file:
+        return len(file.readline())
