36d10ececbd8aec6278ee19932b8d0ac72f878c8
ahmedomi <ahmedomarmiledi@gmail.com>
2020-02-26 09:23:18 +0000

    updating

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    def size(self):
+        self.__size = size
+
+    def __init__(self, size=0):
+        if not type(size) == int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        return pow(self.__size, 2)
