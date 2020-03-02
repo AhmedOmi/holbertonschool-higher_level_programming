0931948ca9a3243e237183cabc55d375f1814c9e
ahmedomi <ahmedomarmiledi@gmail.com>
2020-03-02 21:31:10 +0000

    updating

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXIST second_table(
 id INT,
 name VARCHAR(256),
 score INT);
+-- insert in new table
 INSERT INTO second_table (id, name, score) 
 VALUES(1, 'John', 10),
       (2,'Alex',3),
