5e7ee58581d48994ca14ffa44aaf4107406603d9
ahmedomi <ahmedomarmiledi@gmail.com>
2020-03-02 21:34:23 +0000

    updating

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,7 +5,7 @@ id INT,
 name VARCHAR(256),
 score INT);
 -- insert in new table	
-INSERT INTO second_table
+INSERT INTO second_table (id, name, score)
 VALUES(1, 'John', 10),
       (2,'Alex',3),
       (3,'Bob', 14),
