e8fa2b67dd7d977899de5c6795f3d4f71d5add68
AhmedOmi <ahmedomarmiledi@gmail.com>
2020-03-29 12:22:51 +0100

    updating

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-""" script that lists all states with a name first caracter N """
+""" Filter states by user input """
 import MySQLdb
 from sys import argv
 if __name__ == "__main__":
-    l = "localhost"
-    p = 3306
     a1 = argv[1]
     a2 = argv[2]
     a3 = argv[3]
-    db = MySQLdb.connect(host=l, port=p, user=a1, passwd=a2, db=a3)
+    keyword = argv[4]
+    db = MySQLdb.connect(host="localhost", user=a1, passwd=a2, db=a3)
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states WHERE name\
+    cur.execute("SELECT id, name FROM states WHERE name \
             REGEXP BINARY '{}' ORDER BY id ASC".format(keyword))
-    r = cur.fetchall()
-    for row in r:
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
     db.close()
