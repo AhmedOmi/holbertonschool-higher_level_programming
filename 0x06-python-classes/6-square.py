8a784601086efa6adc0f5ca372c5833e36ff191c
AhmedOmi <ahmedomarmiledi@gmail.com>
2020-04-06 16:55:23 +0000

    6-square.py updating

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -45,6 +45,6 @@ class Square:
                     print("_", end="")
                 for b in range(self.size):
                     print("#", end="")
-                print()
+                print("$")
         else:
             print()
