95cfb8d7bef51878617b9ec1bb3e2f18621ece70
AhmedOmi <ahmedomarmiledi@gmail.com>
2020-04-08 15:34:18 +0000

    10-model_state_my_get.py fixed

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -11,12 +11,12 @@ def get_states():
     """ list all state containe a """
     argument = sys.argv
     base_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
-    db = base_url.format(argument[1], argument[2], argument[3], argument[4])
+    db = base_url.format(argument[1], argument[2], argument[3])
     engine = create_engine(db, pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    x = session.query(State).filter(State.name == arg[4]).first()
+    x = session.query(State).filter(State.name == argument[4]).first()
     if x is not None:
         print(x.id)
     else:
