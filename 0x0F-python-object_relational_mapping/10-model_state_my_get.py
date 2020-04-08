fa407e47aac99e740933a0829a5885d403d110dc
AhmedOmi <ahmedomarmiledi@gmail.com>
2020-04-08 15:25:44 +0000

    10-model_state_my_get.py

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Get a state """
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def get_states():
+    """ list all state containe a """
+    argument = sys.argv
+    base_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}{}'
+    db = base_url.format(argument[1], argument[2], argument[3], argument[4])
+    engine = create_engine(db, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    x = session.query(State).filter(State.name)
+    if(x == argument[4]):
+        print(x.id)
+    else:
+        print("Not found")
+
+
+if __name__ == "__main__":
+    get_states()
