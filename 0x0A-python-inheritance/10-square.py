164647e9ed18f943807cf8159652458602e21ff3
AhmedOmi <ahmedomarmiledi@gmail.com>
2020-05-19 11:12:26 +0100

    10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
-'''import'''
+"""
+import from other file
+"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+class rectangle inheritance from basegeometry
+"""
 
 
 class Rectangle(BaseGeometry):
-    '''rectangle.'''
     def __init__(self, width, height):
-        '''Constructor.'''
+        """
+        constructor
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
-        '''return area'''
+        """
+        return Area
+        """
         return self.__width * self.__height
 
     def __str__(self):
-        '''string.'''
+        """String function"""
         return "[Rectangle] " + \
             str(self.__width) + "/" + str(self.__height)
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -11,7 +11,6 @@ class BaseGeometry:
 
     def area(self):
         raise Exception("area() is not implemented")
-
     """
     function integer validator
     """
