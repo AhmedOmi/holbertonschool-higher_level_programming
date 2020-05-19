3b2ba183f2fea1cb6d92fb229a41d37dc1f59e85
AhmedOmi <ahmedomarmiledi@gmail.com>
2020-05-19 11:17:10 +0100

    11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
+"""
+   Square that inherits from Rectangle (9-rectangle.py).
+   (task based on 10-square.py).
+"""
+
 Rectangle = __import__('9-rectangle').Rectangle
-"""
-import my class Rectangle
-"""
 
 
 class Square(Rectangle):
-    def __init__(self, size):
-        self.__size = size
-        self.integer_validator("size", size)
-        super().__init__(size, size)
+    """
+       Square that inherits from Rectangle (9-rectangle.py).
+       (task based on 10-square.py).
+    """
 
-    def area(self):
-        return self.__size*self.__size
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        Rectangle.__init__(self, size, size)
+        self.__size = size
 
     def __str__(self):
-        return ("[Square] {}/{}".format(self.__size, self.__size))
-
+        """
+           return a string with "[Square] size/size"
+        """
+        return "[Square] %d/%d" % (self.__size, self.__size)
