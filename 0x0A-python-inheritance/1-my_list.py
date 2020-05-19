473138a95efa2523952df9ef0b7923d3b70bc4d1
AhmedOmi <ahmedomarmiledi@gmail.com>
2020-05-19 10:59:25 +0100

    updating

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
+"""
+class inherits from list
+"""
 class MyList(list):
     """
-    class inherits from list
+    prints sorted
     """
-
-    def print_sorted(self):
-        """
-        prints sorted
-        """
+    def print_sorted(self): 
         print(sorted(self))
 
 if __name__ == "__main__":
     import doctest
-    doctest.testfile("tests/1-my_list.txt") 
+    doctest.testfile("tests/1-my_list.txt")
