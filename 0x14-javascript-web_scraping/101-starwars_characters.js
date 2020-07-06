144ac3bcd156559cedd24fb1555583bbabdac037
AhmedOmi <54010086+AhmedOmi@users.noreply.github.com>
2020-07-06 15:41:45 +0100

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,33 +1,25 @@
 #!/usr/bin/node
-const argv = process.argv;
 const request = require('request');
-let character;
-const getChars = {};
-request('https://swapi-api.hbtn.io/api/films/' + argv[2], function (error, request) {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  if (request.statusCode === 200) {
-    const response = JSON.parse(request.body);
-    for (let i = 0; i < response.characters.length; i++) {
-      character = response.characters[i];
-      const request = require('request');
-      request(character, function (error, request) {
-        if (error) {
-          console.log(error);
-          return;
-        }
-        if (request.statusCode === 200) {
-          const response2 = JSON.parse(request.body);
-          getChars[i] = response2.name;
-          if (i === response.characters.length - 1) {
-            for (const c in getChars) {
-              console.log(getChars[c]);
-            }
-          }
-        }
-      });
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+function printCharacters (characters, idx) {
+  request(characters[idx], (err, res, body) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(JSON.parse(body).name);
+      if (idx + 1 < characters.length) {
+        printCharacters(characters, idx + 1);
+      }
     }
+  });
+}
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
