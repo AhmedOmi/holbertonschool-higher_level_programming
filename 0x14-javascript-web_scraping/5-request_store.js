a0cc866b91ffc73c77f220694c0570f2f4ffa03b
AhmedOmi <54010086+AhmedOmi@users.noreply.github.com>
2020-07-06 15:36:16 +0100

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,16 +1,8 @@
 #!/usr/bin/node
-const argv = process.argv;
+const process = require('process');
 const request = require('request');
 const fs = require('fs');
-request(argv[2], function (error, request) {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  if (request.statusCode === 200) {
-    const response = request.body;
-    fs.writeFile(argv[3], response, 'utf-8', err => {
-      if (err) { console.log(err); }
-    });
-  }
+request(process.argv[2], function (error, response, body) {
+  if (error) throw error;
+  fs.writeFile(process.argv[3], body, 'utf8', (err) => { if (err) console.log(err); });
 });
